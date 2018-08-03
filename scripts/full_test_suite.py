f9314e9fc908eae167eb5b0469c2351d309101e8
Steven Jens Jorgensen <stevenjens.m.jorgensen@nasa.gov>
2018-08-03 13:52:27 -0500

    save progress on test suite program

--- scripts/full_test_suite.py
+++ scripts/full_test_suite.py
@@ -29,11 +29,25 @@ import numpy as np
 import rospy
 
 # Define constants
-ACCEPTED_TYPES = ["wholebody_msg", "neck_msg", "footstep_datalist_msg", "foot_load_bearing_msg", "go_home_msg"]
-TEST_STATES = ["read_yaml_file", "update_robot_pose", "update_motion_status", "publish_msg", "wait_for_execution"]
+ACCEPTED_TYPES = ["wholebody", "neck", "walk", "load", "go_home"]
 
+# Define states
+STATE_READ_YAML_FILE = 0
+STATE_UPDATE_ROBOT_POSE = 1
+STATE_PUBLISH_MSG = 2
+STATE_WAIT_FOR_EXECUTION = 3
+STATE_TERMINATE_PROGRAM = 4
+STATE_INDEX_TO_NAME = {0 : "STATE_READ_YAML_FILE", 
+					   1 : "STATE_UPDATE_ROBOT_POSE", 
+					   2 : "STATE_PUBLISH_MSG", 
+					   3 : "STATE_WAIT_FOR_EXECUTION", 
+					   4: "STATE_TERMINATE_PROGRAM"}
+
+# Do not change these default values! If you do, be sure to change the queueable messages as well
 QUEABLE_STARTING_MESSAGE_ID = 100 # message id which indicates this is the start of a queued message 
 QUEABLE_ENDING_MESSAGE_ID = 200 # message id which indicates this is the last message part of the queue
+# A message_id with QUEABLE_STARTING_MESSAGE_ID < message_id < QUEABLE_ENDING_MESSAGE_ID will be processed as fast as possible
+# to send 
 
 class Test_Suite_State_Machine:
 	def __init__(self):
@@ -41,35 +55,43 @@ class Test_Suite_State_Machine:
 		self.robot_pose = pm.Frame(pm.Rotation.Quaternion(0, 0, 0, 1), pm.Vector(0.0, 0.0, 0.0))
 		self.list_of_tests = []
 
-		self.state = "read_yaml_file" # Initialize state to read_yaml_file
-		self.test_index = 0 # Starts at 0
+		self.state = STATE_READ_YAML_FILE # Initialize state to read_yaml_file
+		self.test_index = -1 # Starts at invalid index
 
 		self.current_data = None
 		self.current_message = None
 
 		self.robot_time = None
+		self.update_robot_pose = False
 
 	def load_test_list(self, filename):
+		# Load the test
+
+		# Set index to valid number
+		self.test_index = 0
 		return
 
 	def read_yaml_file(self):
 		return
 
-	def robot_pose_callback(self):
-		# update robot time
+	def robot_pose_callback(self, msg):
+		# Update robot time
+		self.robot_time = msg.header.stamp.to_sec()		
+
 		# if update_robot_pose: 
 			# update robot pose
 			# updated_robot_pose = True
+		
 		return
 
-	def robot_motion_status_callback(self):
+	def robot_motion_status_callback(self, msg):
 		# Check motion status
 		# if pause is requested, do an interrupt and publish stop message
 		# wait()
 		# change state to read yaml
 
 		return		
-	def footstep_status_callback(self):
+	def footstep_status_callback(self, msg):
 		# Check footstep status
 		# If pause is requested. Do an interrupt and publish stop message
 		# wait()
@@ -87,11 +109,37 @@ class Test_Suite_State_Machine:
 		return
 
 	def output_status(self):
-		print "Test Suite state:", self.state
-		print "  test_index = ", self.test_index
+		print "  Running Test", (self.test_index + 1) , " out of ", len(self.list_of_tests), " tests"
+
+	def change_state_to(self, new_state):
+		print "  Changing state to ", STATE_INDEX_TO_NAME[self.state]
+		self.state = new_state
 
 	def run(self):
+		print "Test Suite state:", STATE_INDEX_TO_NAME[self.state]
+
 		# Begin State Machine Logic Loop
+		if self.state == STATE_READ_YAML_FILE:
+			# Check if we have any tests to run
+			if (self.test_index < 0):
+				print "  Warning: No tests to run."
+				self.change_state_to(STATE_TERMINATE_PROGRAM)
+			else:
+
+				# We have Programs to run
+				if self.test_index < len(self.list_of_tests):
+					print('Reading YAML file')		
+				else:
+					self.change_state_to(STATE_TERMINATE_PROGRAM)
+
+
+		elif (self.state == STATE_TERMINATE_PROGRAM):
+			print "    Terminating program"
+			return False
+
+
+
+		# Pseudo Code:
 		'''
 			if state == read_yaml:
 				if test_index < total_tests:
@@ -99,7 +147,7 @@ class Test_Suite_State_Machine:
 				else:
 					state = Done
 
-				if ((message_id > QUEABLE_STARTING_MESSAGE_ID) and (message_id < QUEABLE_ENDING_MESSAGE_ID)):
+				if (message_id > QUEABLE_STARTING_MESSAGE_ID) and (message_id < QUEABLE_ENDING_MESSAGE_ID)):
   				 	perform transforms using previous pose, publish, wait for 1 second then read the next file.
 					change state to read_yaml.
 					Read the file. and immediately publish the message
@@ -140,12 +188,17 @@ class Test_Suite_State_Machine:
 
 		'''
 
-		return self.state
+		return True
 
 
 
 if __name__ == '__main__':
 	rospy.init_node('ValkyrieTestSuite')
+
+	# if not rospy.has_param('~TestSuiteDataFile'):
+	# 	print('Please specify the test suite data file (YAML)!')
+	# else:	
+	# 	run_state_machine()
 
 	test_suite_obj = Test_Suite_State_Machine()
 
@@ -166,8 +219,10 @@ if __name__ == '__main__':
 	# # List of YAML files
 	# # Read YAML file
 
+	test_suite_obj.load_test_list('')
 	while not rospy.is_shutdown():
-		print "state machine loop"
-		test_suite_obj.run()
-		time.sleep(0.5)
+		if test_suite_obj.run():
+			time.sleep(0.5)
+		else:
+			break
 	print('Done')
